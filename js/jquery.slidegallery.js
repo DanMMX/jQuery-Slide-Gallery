ee6aa14e5b3303b5fcc92ea010673f8e4aed5095
DanMMX <kabezon_91@hotmail.com>
2013-11-07 10:20:10 -0600

    Update jquery.slidegallery.js

--- js/jquery.slidegallery.js
+++ js/jquery.slidegallery.js
@@ -10,31 +10,72 @@
 					$sliderOut = $slidegallery.find(settings.sliderOut),
 					$sliderIn = $sliderOut.find(settings.sliderIn),
 					$controls = $slidegallery.find(settings.controls),
+					$others = $slidegallery.find(settings.others),
 					$left = $controls.find(".left"),
 					$right = $controls.find(".right"),
-					buttonAction = settings.buttonAction;
+					buttonAction = settings.buttonAction,
+					p = settings.percentages;
 
 				//Configuración
-
+				
 				if($sliderIn.length <= 0 && $sliderOut.length <= 0){
-					$sliderIn = $slidegallery.children().not($controls).wrapAll("<div></div>").parent().addClass("sliderIn");
+					$sliderIn = $slidegallery.children().not($controls).not($others).wrapAll("<div></div>").parent().addClass("sliderIn");
 					$sliderOut = $sliderIn.wrap("<div></div>").parent().addClass("sliderOut");
 				}
 
-				$sliderOut.css({
-					width : settings.width + settings.widthUnit,
-					height : settings.height + settings.heightUnit,
-					overflow : "hidden"
-				});
+				if(!p)
+					$sliderOut.css({
+						width : settings.width + settings.widthUnit,
+						height : settings.height + settings.heightUnit,
+						overflow : "hidden"
+					});
+				else
+					$sliderOut.css({						
+						width : settings.width + "%",
+						height : "auto",
+						overflow : "hidden"
+					});
+
+				if(settings.fixOthers){
+					if(!p)
+						$others.children().css({
+							width : (settings.width - 20) + settings.widthUnit,
+							"min-height" : (settings.height - 20) + settings.heightUnit,
+							padding : 10 + "px"
+						});
+					else
+						$others.children().css({
+							width : settings.width + "%",
+							padding : 10 + "px",						
+							"-webkit-box-sizing": "border-box",
+							"-moz-box-sizing": "border-box",
+							"-o-box-sizing": "border-box",
+							"box-sizing": "border-box"
+						});
+				}
 				
 				$sliderIn.find(":first-child").clone().appendTo($sliderIn);
 				
-				$sliderIn.css({
-					width : settings.width * $sliderIn.children().length + settings.widthUnit,
-					height : settings.height + settings.heightUnit,
-					position : "relative",
-					right : 0 + "px"
-				});
+				if(!p)
+					$sliderIn.css({
+						width : settings.width * $sliderIn.children().length + settings.widthUnit,
+						height : settings.height + settings.heightUnit,
+						position : "relative",
+						right : 0 + "px"
+					});
+				else{
+					$sliderIn.css({
+						width : 100 * $sliderIn.children().length + "%",
+						height : 100 + "%",
+						position : "relative",
+						right : 0 + "%"
+					});
+
+					$sliderIn.children().css({
+						width: 100 / $sliderIn.children().length + "%",
+					})
+				}
+
 
 				if($controls.length <= 0){
 					$controls = $("<div />").addClass("controls");
@@ -61,9 +102,18 @@
 				//Funcionalidad
 				$right.click(function(){
 					if($($slidegallery).filter(":animated").length == 0){
-						var position = Math.round(parseInt($sliderIn.css("right")) / settings.width);
-						var children = $sliderIn.children().length;
+						var position;
+						var r = $sliderIn.css("right");
+						if(!p)
+							position = Math.round(parseFloat($sliderIn.css("right")) / settings.width);
+						else if(r.indexOf("%") == -1 ){
+							position = Math.round((parseFloat($sliderIn.css("right"))/parseFloat($sliderIn.parent().width())));
+						}else{
+							position = Math.round((parseFloat($sliderIn.css("right"))/100));
+						}
 
+						var children = $sliderIn.children().length;
+						
 						if(typeof buttonAction == "function"){
 							var actualPosition = position;
 							if(actualPosition == children - 2)
@@ -78,21 +128,39 @@
 						position ++;
 						
 						if(position == $sliderIn.children().length){
-							$sliderIn.css({
-								right : 0 + "px"
-							});
+							if(!p)
+								$sliderIn.css({
+									right : 0 + "px"
+								});
+							else
+								$sliderIn.css({
+									right : 0 + "%"
+								});
 							position = 1;
 						}
-						
-						$sliderIn.animate({
-							right : position * settings.width + settings.widthUnit
-						})
+						if(!p)
+							$sliderIn.animateCss({
+								right : position * settings.width + settings.widthUnit
+							})
+						else
+							$sliderIn.animateCss({
+								right : position * 100 + "%"
+							})
+
 					}
 				})
-
+				
 				$left.click(function(){
 					if($($slidegallery).filter(":animated").length == 0){
-						var position = Math.round(parseInt($sliderIn.css("right")) / settings.width);
+						var position;						
+						if(!p)
+							position = Math.round(parseFloat($sliderIn.css("right")) / settings.width);
+						else if(r.indexOf("%") == -1 ){
+							position = Math.round((parseFloat($sliderIn.css("right"))/parseFloat($sliderIn.parent().width())));
+						}else{
+							position = Math.round((parseFloat($sliderIn.css("right"))/100));
+						}
+						
 						var children = $sliderIn.children().length;
 
 						if(typeof buttonAction == "function"){
@@ -108,20 +176,30 @@
 
 						if(position < 0){						
 							position = $sliderIn.children().length - 1;
-							$sliderIn.css({
-								right : position * settings.width + settings.widthUnit
-							});
+							if(!p)
+								$sliderIn.css({
+									right : position * settings.width + settings.widthUnit
+								});
+							else
+								$sliderIn.css({
+									right : position * 100 + "%"
+								});
 							position --;						
 						}
 
-						$sliderIn.animate({
-							right : position * settings.width + settings.widthUnit
-						})
+						if(!p)
+							$sliderIn.animateCss({
+								right : position * settings.width + settings.widthUnit
+							})
+						else							
+							$sliderIn.animateCss({
+								right : position * 100 + "%"
+							})
 					}
 				});
 
 				var hashVal = window.location.hash.split("#")[1];
-				if(hashVal !== "undefined"){
+				if(hashVal){
 					var position = parseInt(hashVal);
 					var children = $sliderIn.children().length;
 					
@@ -131,23 +209,35 @@
 							actualPosition = children - 1;
 						
 						actualPosition --;
-						buttonAction(actualPosition)
+						buttonAction(actualPosition);
 					}
 
 					if(position < 0){						
 						position = $sliderIn.children().length - 1;
-						$sliderIn.css({
-							right : position * settings.width + settings.widthUnit
-						});
-						position --;						
+						if(!p)
+							$sliderIn.css({
+								right : position * settings.width + settings.widthUnit
+							});
+						else
+							$sliderIn.css({
+								right : position * 100 + "%"
+							});
+						position --;
 					}
 
-					$sliderIn.animate({
-						right : position * settings.width + settings.widthUnit
-					})
+					if(!p)
+						$sliderIn.animateCss({
+							right : position * settings.width + settings.widthUnit
+						})
+					else
+						$sliderIn.animateCss({
+							right : position * 100 + "%"
+						})
 					$(document).scrollTop(0);
 				}
-				
+
+				if(settings.callback)
+					settings.callback();				
 			});
 		},
 		destroy : function() {
@@ -180,7 +270,11 @@
 		"widthUnit" : "px",
 		"height" : 250,
 		"heightUnit" : "px",
-		"buttonAction" : function(){}
+		"buttonAction" : function(){},
+		"3d" : false,
+		"others" : ".other",
+		"fixOthers" : true,
+		"percentages" : false
 	}
 	
 })(jQuery);
